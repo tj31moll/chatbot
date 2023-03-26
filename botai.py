b8e4c857f3d2975f35e3bc85257784e3b49c6a14
tj31moll <91799649+tj31moll@users.noreply.github.com>
2023-03-26 18:59:35 -0400

    Update botai.py

--- botai.py
+++ botai.py
@@ -1,48 +1,113 @@
+import os
+import re
+import sqlite3
 import logging
-import random
-from transformers import GPT2LMHeadModel, GPT2Tokenizer
+import matplotlib.pyplot as plt
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
 
 # Set up logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 # Load GPT-2 model and tokenizer
 model = GPT2LMHeadModel.from_pretrained("gpt2")
 tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
 
-# Your Telegram bot token
-TOKEN = "YOUR_TELEGRAM_BOT_TOKEN_HERE"
+# Set up SQLite database
+def setup_database():
+    conn = sqlite3.connect("memory.db")
+    cur = conn.cursor()
+    cur.execute('''CREATE TABLE IF NOT EXISTS memory
+                   (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    key TEXT,
+                    value TEXT)''')
+    conn.commit()
+    return conn
+
+conn = setup_database()
+memory = {}
 
 def start(update: Update, context: CallbackContext):
-    update.message.reply_text("Hi! I'm a self-training Telegram bot. Send me a message, and I'll try to respond!")
-
-def generate_response(input_text):
-    input_ids = tokenizer.encode(input_text, return_tensors='pt')
-    response_ids = model.generate(input_ids, max_length=100, num_return_sequences=1)
-    response = tokenizer.decode(response_ids[0], skip_special_tokens=True)
-    return response
+    update.message.reply_text("Hi! I'm a self-training Telegram bot. You can talk to me or ask me to remember something for you.")
 
 def handle_message(update: Update, context: CallbackContext):
-    input_text = update.message.text
-    response = generate_response(input_text)
+    global conn
+    input_text = update.message.text.lower()
+
+    if input_text.startswith("what did i"):
+        key = input_text[10:].strip()
+        cur = conn.cursor()
+        cur.execute("SELECT value FROM memory WHERE key LIKE ?", ('%' + key + '%',))
+        result = cur.fetchone()
+        if result:
+            update.message.reply_text(f"You said: {result[0]}")
+        else:
+            update.message.reply_text(f"Sorry, I don't remember {key}.")
+        return
+
+    response = generate_response(update.message.text)
     update.message.reply_text(response)
 
-def error(update: Update, context: CallbackContext):
-    logger.warning('Update "%s" caused error "%s"', update, context.error)
+def generate_response(input_text: str):
+    input_ids = tokenizer.encode(input_text, return_tensors='pt')
+    output = model.generate(input_ids, max_length=50, num_return_sequences=1, no_repeat_ngram_size=2, early_stopping=True)
+    response = tokenizer.decode(output[0], skip_special_tokens=True)
+    return response
+
+def remember(update: Update, context: CallbackContext):
+    global conn
+    input_text = update.message.text.lower()
+
+    pattern = r"remember (that|) (i|) (.*?)$"
+    match = re.search(pattern, input_text)
+    if match:
+        key = " ".join([match.group(1), match.group(2), match.group(3)]).strip()
+        cur = conn.cursor()
+        cur.execute("INSERT INTO memory (key, value) VALUES (?, ?)", (key, input_text))
+        conn.commit()
+        update.message.reply_text(f"I'll remember {input_text}.")
+    else:
+        update.message.reply_text("Sorry, I couldn't understand your request. Please use the format 'remember [your message]'.")
+
+def generate_chart(update: Update, context: CallbackContext):
+    global conn
+    cur = conn.cursor()
+    cur.execute("SELECT key, value FROM memory")
+    results = cur.fetchall()
+
+    keys = [x[0] for x in results]
+    values = [x[1] for x in results]
+
+    plt.barh(keys, values)
+    plt.xlabel('Keys')
+    plt.ylabel('Values')
+    plt.title('Memory Chart')
+    plt.tight_layout()
+
+    chart_path = "memory_chart.png"
+    plt.savefig(chart_path)
+    plt.clf()
+
+    update.message.reply_photo(open(chart_path, "rb"))
 
 def main():
-    updater = Updater(TOKEN, use_context=True)
+    # Replace 'YOUR_API_TOKEN' with your Telegram bot's API token
+    updater = Updater("YOUR_API_TOKEN", use_context=True)
+
     dp = updater.dispatcher
 
+    # Add command and message handlers
     dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("chart", generate_chart))
+    dp.add_handler(MessageHandler(Filters.regex(r'(?i)remember .*') & ~Filters.command, remember))
     dp.add_handler(MessageHandler(Filters.text & ~Filters.command, handle_message))
 
-    dp.add_error_handler(error)
-
+    # Start the bot
     updater.start_polling()
+
+    # Run the bot until Ctrl-C is pressed or the process receives SIGINT, SIGTERM or SIGABRT
     updater.idle()
 
 if __name__ == '__main__':
